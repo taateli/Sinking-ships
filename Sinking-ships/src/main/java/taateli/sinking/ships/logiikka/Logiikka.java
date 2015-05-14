9d27af2d18fcbf848f968417f63d812b87939868
Tatu Helander <tatuhelander@dhcp-asv-98.helsinki.fi>
2015-05-14 18:35:30 +0300

    Vähän lisää koodia

--- Sinking-ships/src/main/java/taateli/sinking/ships/logiikka/Logiikka.java
+++ Sinking-ships/src/main/java/taateli/sinking/ships/logiikka/Logiikka.java
@@ -12,29 +12,31 @@ public class Logiikka {
     private final int[][] lauta;
 
     public Logiikka() {
-        this.lauta = new int[5][5];
+        this.lauta = new int[6][6];
     }
 
     public void alustaLauta() {
-        for (int rivi = 0; rivi < 5; rivi++) {
-            for (int sarake = 0; sarake < 5; sarake++) {
+        for (int rivi = 0; rivi < 6; rivi++) {
+            for (int sarake = 0; sarake < 6; sarake++) {
                 lauta[rivi][sarake] = -1;
             }
         }
     }
 
     public void tulostaPeliAlue() {
-        System.out.println("\t1 \t2 \t3 \t4 \t5");
+        System.out.println("\t1 \t2 \t3 \t4 \t5 \t6");
         System.out.println();
 
-        for (int rivi = 0; rivi < 5; rivi++) {
+        for (int rivi = 0; rivi < 6; rivi++) {
             System.out.print((rivi + 1) + "");
-            for (int sarake = 0; sarake < 5; sarake++) {
+            for (int sarake = 0; sarake < 6; sarake++) {
                 if (lauta[rivi][sarake] == -1) {
                     System.out.print("\t" + "~");
                 } else if (lauta[rivi][sarake] == 0) {
                     System.out.print("\t" + "*");
                 } else if (lauta[rivi][sarake] == 1) {
+                    System.out.print("\t" + "=");
+                } else if (lauta[rivi][sarake] == 2) {
                     System.out.print("\t" + "X");
                 }
 
@@ -44,19 +46,20 @@ public class Logiikka {
 
     }
 
-    public void asetaLaiva(int koko, int suunta, int rivi, int sarake) {
-        if (suunta == 1) {   // suunta horisontaalinen eli vaaka
+    public void asetaLaiva(int koko, boolean horisontaalinen, int rivi, int sarake) {
+
+        if (horisontaalinen == true) {   // suunta horisontaalinen
             int onkoLaudallaHorisontaalisesti = koko + sarake;
-            if (onkoLaudallaHorisontaalisesti < 5) {
+            if (onkoLaudallaHorisontaalisesti < 6) {
                 for (int i = 0; i < koko; i++) {
                     lauta[rivi][sarake] = 1;
                     sarake++;
                 }
 
             }
-            if (suunta == 0) {   // suunta vertikaalinen eli 
+        } else {   // suunta vertikaalinen
             int onkoLaudallaVertikaalisesti = koko + rivi;
-            if (onkoLaudallaVertikaalisesti < 5) {
+            if (onkoLaudallaVertikaalisesti < 6) {
                 for (int i = 0; i < koko; i++) {
                     lauta[rivi][sarake] = 1;
                     rivi++;
@@ -66,4 +69,13 @@ public class Logiikka {
 
         }
     }
+
+    public void osuukoLaivaan(int rivi, int sarake) {
+        if (lauta[rivi][sarake] == 1) {
+            lauta[rivi][sarake] = 2;
+        } else {
+            lauta[rivi][sarake] = 0;
+        }
+    }
+
 }
--- Sinking-ships/src/main/java/taateli/sinking/ships/logiikka/Paaluokka.java
+++ Sinking-ships/src/main/java/taateli/sinking/ships/logiikka/Paaluokka.java
@@ -13,7 +13,9 @@ public class Paaluokka {
     public static void main(String[] args) {
         Logiikka logic = new Logiikka();
         logic.alustaLauta();
-        logic.asetaLaiva(2, 0, 2, 1);
+        logic.asetaLaiva(2, false, 2, 0);
+        logic.asetaLaiva(3, true, 1, 1);
+        logic.osuukoLaivaan(2, 0);
         logic.tulostaPeliAlue();
     }
 }
